91a6a117db1185bfb5ac76a1c589861c7fd1840a
shiv1810 <92572208+shiv1810@users.noreply.github.com>
2024-03-01 00:58:03 +0530

    Update app.py

--- SPPA-API/app.py
+++ SPPA-API/app.py
@@ -37,5 +37,5 @@ def display_prediction():
 
     return jsonify(result)
     
-if __name__ == "__main__":
-    app.run(debug=False)
+ if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
